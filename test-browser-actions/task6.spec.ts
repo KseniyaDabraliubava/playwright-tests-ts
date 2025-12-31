48f142243bb93e7027e08571a42a4c6b3df33685
Kseniya Dabraliubava <kseniyadabraliubava@k-dabraliubava.local>
2025-12-31 21:29:25 +0100

    HW23

--- test-browser-actions/task6.spec.ts
+++ test-browser-actions/task6.spec.ts
@@ -1,10 +1,16 @@
 import { test, expect } from "@playwright/test";
+import path from 'path';
 
-test('Task6: Check file upload', async ({ page }) => {
+
+test('Test6: Check file upload', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/upload');
-  
-  await page.locator('#file-upload').setInputFiles('test.txt');
+
+  await page
+    .locator('#file-upload')
+    .setInputFiles(path.resolve(__dirname, 'files/test.txt'));
+
   await page.locator('#file-submit').click();
-  
+
+  await expect(page.locator('h3')).toHaveText('File Uploaded!');
   await expect(page.locator('#uploaded-files')).toHaveText('test.txt');
 });
